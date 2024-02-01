ef9294f8e232977414f64cb43e1242ab65ddf318
asjer bereket <asjer.abraham@yahoo.com>
2024-02-01 22:32:15 +0100

    Lærer meg Java, jobber på syntax og variabler

---
+++ Task.java
@@ -0,0 +1,9 @@
+public class Task {
+    public static void main(String[] args) {
+        String FirstName = "John ";
+        String LastName = "Doe ";
+        String FullName = FirstName + LastName;
+        System.out.println(FullName);
+
+    }
+}
